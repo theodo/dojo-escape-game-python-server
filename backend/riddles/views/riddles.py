61da4689a5d75e58d1f4a6053871c5925b2ae0d5
leoa <leoa@theodo.fr>
2019-10-08 10:59:02 +0200

    feat(backend): create route to associate user to ids

--- backend/riddles/views/riddles.py
+++ backend/riddles/views/riddles.py
@@ -1,3 +1,5 @@
+import random
+
 from core.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -20,3 +22,18 @@ def upgrade_to_level_1(request):
     user.save()
 
     return Response({}, status=200)
+
+
+@api_view(["GET"])
+def get_username(request, player_id):
+    try:
+        user = User.objects.get(player_id=player_id)
+        return Response(user.username, status=200)
+    except User.DoesNotExist:
+        free_users = User.objects.filter(player_id=None, is_staff=False)
+        user_index = random.randint(0, len(free_users))
+        user = free_users[user_index]
+        user.player_id = player_id
+        user.save()
+
+        return Response(user.username, status=200)
