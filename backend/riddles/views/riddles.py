ca86d4a3517c3509da1c13936e2d686847e17c7d
leoa <leoa@theodo.fr>
2019-10-09 12:23:59 +0200

    feat(api): create route for us to get the json

--- backend/riddles/views/riddles.py
+++ backend/riddles/views/riddles.py
@@ -1,8 +1,12 @@
 import random
 
+import jwt
+
 from core.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+SECRET = "MY_AWESOME_SECRET"
 
 
 @api_view(["POST"])
@@ -37,3 +41,19 @@ def get_username(request, player_id):
         user.save()
 
         return Response(user.username, status=200)
+
+
+@api_view(["GET"])
+def get_json(request):
+    users = User.objects.filter(is_staff=False)
+    response = {}
+    for user in users:
+        username = user.username
+        for i in range(10000):
+            token = jwt.encode(
+                {"player_id": i, "username": username}, SECRET, algorithm="HS256"
+            )
+            response[
+                f"{username}-{i}"
+            ] = f"{request.build_absolute_uri('/api/get-hint/')}?token={token.decode('ascii')}"  # .decode is needed because py-jwt return a binary string
+    return Response(response, status=200)
