02631b3fb27effdaf5ccbcf96a10719283d7da78
jordanl <jordanl@theodo.fr>
2019-10-16 10:30:06 +0200

    Add ip_address field to user model

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth.admin import UserAdmin
 
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
-        (None, {"fields": ("level", "player_id", "hint")}),
+        (None, {"fields": ("level", "player_id", "hint", "ip_address")}),
     ) + UserAdmin.fieldsets
 
 
