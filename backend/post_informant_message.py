c0bb6bb821b426fadc1ce41b06f84078106d0eb7
aurelien <ojisama.san@gmail.com>
2019-10-21 14:40:55 +0200

    jordan's review

--- backend/post_informant_message.py
+++ backend/post_informant_message.py
@@ -13,19 +13,19 @@ class colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-BASE_URL = "http://localhost:8000"
+BASE_URL = "http://ripper.theo.do"
 SLEEP_TIMER = 20
 ADMIN_SECRET = "the_killer_is_colonel_custard"
 SPOTIFY_HINT = "ID DE PLAYLIST"
 BASE_MESSAGE_CONTENT = """
-Ok {}, j'ai des infos pour toi.
+Ok {}, j'ai des infos pour vous.
 Un portable a été retrouvé sur une des scènes de crime.
 Il était sous scellés au commissariat, mais il a disparu depuis quelques heures.
 Le voleur a sûrement voulu effacer des preuves sur le téléphone, donc il a un lien avec les meurtres.
 Heureusement, on avait mis un mouchard dessus qui a capté quelques octets de données.
 Les données en arrière-plan, tout le monde oublie de les désactiver. Une chance pour nous !
-On sait que la chaîne de caractère {} a été envoyé aux serveurs de Spotify.
-Le voleur a sûrement un lien avec le(s) tueur(s). Retrouvez qui a utilisé le téléphone en retrouvant à quoi correspond {}.
+On sait que la chaîne de caractères "{}" a été envoyée aux serveurs de Spotify.
+Le voleur a sûrement un lien avec le(s) tueur(s). Trouvez qui a utilisé le téléphone en retrouvant à quoi correspond "{}".
 """
 
 while True:
@@ -34,7 +34,7 @@ while True:
     for username, ip in ips.items():
         print(f"Posting message to user {username} (IP : {ip})")
         try:
-            response = requests.post(f"http://{ip}:8000", data=BASE_MESSAGE_CONTENT.format(username, SPOTIFY_HINT, SPOTIFY_HINT), timeout=3)
+            response = requests.post(f"http://{ip}:5000", data=BASE_MESSAGE_CONTENT.format(username, SPOTIFY_HINT, SPOTIFY_HINT), timeout=3)
             assert response.status_code == 200
             print(colors.OKGREEN + f"Successfully posted message to user {username} (IP : {ip})" + colors.ENDC)
         except:
